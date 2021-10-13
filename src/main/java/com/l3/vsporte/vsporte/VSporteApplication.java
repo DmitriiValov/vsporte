6d8579abc4f6e3e637719d010959c36ee0029bd7
Dmitry Valov <d_valov@mail.ru>
2021-10-13 16:46:44 +0600

    Added SpringBoot support

---
+++ src/main/java/com/l3/vsporte/vsporte/VSporteApplication.java
@@ -0,0 +1,13 @@
+package com.l3.vsporte.vsporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VSporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VSporteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/l3/vsporte/vsporte/VSporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l3.vsporte.vsporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VSporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
